fc0e439203ec392c9761e714ca2b6dd380c5bf6b
Shane Cunningham <ShaneCunn@users.noreply.github.com>
2017-05-31 13:17:32 +0100

    php curl to git hub api  avatar image

--- GiThub/curl.php
+++ GiThub/curl.php
@@ -11,5 +11,10 @@ $username = 'shanecunn';
 $url = 'https://api.github.com/users/shanecunn';
 $process = curl_init($url);
 curl_setopt($process, CURLOPT_USERAGENT, $username);
-curl_exec($process);
+curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+
+$return = curl_exec($process);
+$results = json_decode($return);
+//var_dump($results);
+echo '<img src="' . $results->avatar_url . '" />';
 curl_close($process);
