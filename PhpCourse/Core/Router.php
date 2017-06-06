b7bf614c6cb67d6bd004545f4ba2e4a2c08a3a6b
Shane Cunningham <ShaneCunn@users.noreply.github.com>
2017-06-06 13:56:10 +0100

    added router

---
+++ PhpCourse/Core/Router.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: MTA USER
+ * Date: 06/06/2017
+ * Time: 13:53
+ */
+class Router
+{
+
+}
---
+++ PhpCourse/public/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MTA USER
+ * Date: 06/06/2017
+ * Time: 12:39
+ *
+ * Front Controller
+ *
+ */
+//echo "Requested URL= ". $_SERVER['QUERY_STRING'];
+//echo "hello shane";
+
+require '../Core/Router.php';
+
+$router = new Router();
+
+echo  get_class($router);
