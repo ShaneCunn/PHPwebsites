9d21c959a50ebfb0b55dbaf581648d6d01633cdf
Shane Cunningham <ShaneCunn@users.noreply.github.com>
2017-06-06 14:09:31 +0100

    add routes

--- PhpCourse/Core/Router.php
+++ PhpCourse/Core/Router.php
@@ -8,5 +8,22 @@
  */
 class Router
 {
+ protected  $routes = [];
 
+
+ public function add($route, $params){
+
+     $this-> routes[$route] = $params;
+ }
+
+
+    /**
+     *
+     *  get
+     * @return array
+     */
+    public function getRoutes(): array
+    {
+        return $this->routes;
+    }
 }
--- PhpCourse/public/index.php
+++ PhpCourse/public/index.php
@@ -16,3 +16,11 @@ require '../Core/Router.php';
 $router = new Router();
 
 echo  get_class($router);
+
+$router->add('',['controller'=>'Home', 'action' => 'index']);
+$router->add('posts',['controller'=>'Post', 'action' => 'index']);
+$router->add('posts/new',['controller'=>'Post', 'action' => 'index']);
+
+echo '<pre>';
+var_dump($router->getRoutes());
+echo '</pre>';
